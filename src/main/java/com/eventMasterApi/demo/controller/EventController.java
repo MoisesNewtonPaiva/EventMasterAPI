24bdf97e34daf8a495035921dede4981c5fc1557
MoisesNewtonPaiva <12123005@aluno.newtonpaiva.br>
2026-03-25 13:28:46 -0300

    Altera anotação de @Controller para @RestController na classe EventController

--- src/main/java/com/eventMasterApi/demo/controller/EventController.java
+++ src/main/java/com/eventMasterApi/demo/controller/EventController.java
@@ -4,7 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
 
 import com.eventMasterApi.demo.DTO.EventDTO;
 import com.eventMasterApi.demo.infrastructure.service.EventService;
@@ -12,11 +12,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 
-@Controller
+@RestController
 public class EventController {
     
 
